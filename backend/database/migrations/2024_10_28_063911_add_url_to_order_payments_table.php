bd6afc49619c288ca513ab925bbb856f21dd0309
felix <nguyenquockhanhmusic@gmail.com>
2024-10-28 15:18:43 +0700

    Orderupdate

---
+++ backend/database/migrations/2024_10_28_063911_add_url_to_order_payments_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('order_payments', function (Blueprint $table) {
+            $table->string('url')->nullable()->after('status');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('order_payments', function (Blueprint $table) {
+            $table->dropColumn('url');
+        });
+    }
+};
