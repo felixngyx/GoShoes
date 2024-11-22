d28bc7875f6e324eddc176bbdc97115cb5372af9
Chi Viet <vietchi296@gmail.com>
2024-11-22 09:04:59 +0700

    CRUD product

--- backend/app/Repositories/ProductRepository.php
+++ backend/app/Repositories/ProductRepository.php
@@ -53,28 +53,30 @@ class ProductRepository implements ProductRepositoryInterface
             'stock_quantity' => $product->stock_quantity,
             'sku' => $product->sku,
             'hagtag' => $product->hagtag,
-            'brand' => $product->brand->name,
+            'brand_id' => $product->brand_id,
+            // 'brand' => $product->brand->name,
             'rating_count' => $product->rating_count,
             'status' => $product->status,
             'thumbnail' => $product->thumbnail,
             'images' => $product->images->map(function ($image) {
                 return [
                     'id' => $image->id,
-                    'image' => $image->image_path
+                    'image_path' => $image->image_path
                 ];
             }),
-            'categories' => $product->categories->map(function ($category) {
-                return [
-                    'id' => $category->id,
-                    'name' => $category->name
-                ];
-            }),
+            'category_ids' => $product->categories->pluck('id')->toArray(),
+            // 'categories' => $product->categories->map(function ($category) {
+            //     return [
+            //         'id' => $category->id,
+            //         'name' => $category->name
+            //     ];
+            // }),
             'variants' => $product->variants->map(function ($variant) {
                 return [
                     'size_id' => $variant->size_id,
                     'color_id' => $variant->color_id,
-                    'size' => (int) $variant->size->size,
-                    'color' => $variant->color->color,
+                    // 'size' => (int) $variant->size->size,
+                    // 'color' => $variant->color->color,
                     'quantity' => $variant->quantity,
                     // 'image_variant' => $variant->image_variant
                 ];
@@ -131,10 +133,10 @@ class ProductRepository implements ProductRepositoryInterface
             ->where('id', '!=', $product->id)
             ->where('is_deleted', false)
             ->limit(8)
-            
+
             ->get();
 
-        
+
         return [
             'product' => $product,
             'relatedProducts' => $relatedProducts
@@ -152,7 +154,7 @@ class ProductRepository implements ProductRepositoryInterface
     }
 
     public function find($id)
-    { 
+    {
         return Product::where('is_deleted', false)->findOrFail($id);
     }
     public function checkStockProductVariant($id)
@@ -174,8 +176,8 @@ class ProductRepository implements ProductRepositoryInterface
         //     ];
         // });
         // $brandName = $product->brand ? $product->brand->name : null;
-        
-        // $categoryNames = $product->categories->pluck('name')->toArray(); 
+
+        // $categoryNames = $product->categories->pluck('name')->toArray();
    // 'variantDetails' => $variantDetails,
             // 'brandName' => $brandName,
             // 'categoryNames' => $categoryNames, // Lấy tên danh mục sản phẩm
--- frontend/src/services/admin/product.ts
+++ frontend/src/services/admin/product.ts
@@ -2,34 +2,23 @@ import axiosClient from '../../apis/axiosClient';
 import { CATEGORY } from './category';
 
 export type PRODUCT = {
-	id?: number;
 	name: string;
 	description: string;
 	price: number;
+	stock_quantity: number;
 	promotional_price: number;
 	status: string;
 	sku: string;
 	hagtag: string;
 	category_ids: number[];
-	brand_id: number;
 	thumbnail: string;
-	images: string[];
-	stock_quantity: number;
+	brand_id: number;
 	variants: {
-		color: string; // Red, Blue, Green
-		size_id: number; // 1, 2, 3
-		quantity: number; // 10, 20, 30
-		image_variant: string; // https://res.cloudinary.com/dxxwcby8z/image/upload/v1716262513/product/1726764884888_1726764884888.jpg
+		color_id: number;
+		size_id: number;
+		quantity: number;
 	}[];
-};
-
-export type Variant = {
-	size_id: number;
-	color_id: number;
-	size: number;
-	color: string;
-	quantity: number;
-	image_variant: string;
+	images: string[];
 };
 
 export type PRODUCT_DETAIL = {
@@ -41,27 +30,8 @@ export type PRODUCT_DETAIL = {
 	stock_quantity: number;
 	sku: string;
 	hagtag: string;
-	brand: number;
-	status: string;
-	thumbnail: string;
-	images: {
-		id: number;
-		image_path: string;
-	}[];
-	categories: CATEGORY[];
-	variants?: Variant[];
-};
-
-export type PRODUCT_UPDATE = {
-	id?: number;
-	name: string;
-	description: string;
-	price: number;
-	promotional_price: number;
-	stock_quantity: number;
-	sku: string;
-	hagtag: string;
 	brand_id: number;
+	rating_count: number;
 	status: string;
 	thumbnail: string;
 	images: {
@@ -69,13 +39,36 @@ export type PRODUCT_UPDATE = {
 		image_path: string;
 	}[];
 	category_ids: number[];
-	variants?: {
+	variants: {
 		color_id: number;
 		size_id: number;
 		quantity: number;
-		image_variant: string;
 	}[];
 };
+
+// export type PRODUCT_UPDATE = {
+// 	id?: number;
+// 	name: string;
+// 	description: string;
+// 	price: number;
+// 	promotional_price: number;
+// 	stock_quantity: number;
+// 	sku: string;
+// 	hagtag: string;
+// 	brand_id: number;
+// 	status: string;
+// 	thumbnail: string;
+// 	images: {
+// 		id?: number;
+// 		image_path: string;
+// 	}[];
+// 	category_ids: number[];
+// 	variants?: {
+// 		color_id: number;
+// 		size_id: number;
+// 		quantity: number;
+// 	}[];
+// };
 
 const productService = {
 	getAll: (page: number = 1, limit: number = 5) => {
@@ -83,14 +76,14 @@ const productService = {
 			`/products?page=${page}&limit=${limit}&order_by=created_at&order=desc`
 		);
 	},
-	getById: async (id: number) => {
+	getById: async (id: number): Promise<PRODUCT_DETAIL> => {
 		const res = await axiosClient.get(`/productDetail/${id}`);
 		return res.data.Data.product;
 	},
 	create: (data: PRODUCT) => {
 		return axiosClient.post('/products', data);
 	},
-	update: (id: number, data: PRODUCT_UPDATE) => {
+	update: (id: number, data: PRODUCT_DETAIL) => {
 		return axiosClient.put(`/products/${id}`, data);
 	},
 	delete: (id: number) => {
