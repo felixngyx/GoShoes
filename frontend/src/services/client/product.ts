d0fc3bac956513b9a83b12f6a97da86cab7d42e7
youngchinh <bachinh456@gmail.com>
2024-11-22 16:45:38 +0700

    comit

--- frontend/src/services/client/product.ts
+++ frontend/src/services/client/product.ts
@@ -55,3 +55,13 @@ export const checkStock = async (variant_id: number): Promise<number> => {
     throw error;
   }
 };
+
+export const getProductByBrandId = async (brandId: number) => {
+  try {
+    const response = await axiosClient.get(`/products?brand_id=${brandId}`);
+    return response.data.data.products;
+  } catch (error) {
+    console.error("Error fetching products by brand ID:", error);
+    throw new Error("Failed to fetch products by brand ID");
+  }
+};
---
+++ frontend/src/types/client/brands.ts
@@ -0,0 +1,7 @@
+export interface Brand {
+  id: string;
+  name: string;
+  logo_url: string;
+  average_rating: number;
+  products_count: number;
+}
