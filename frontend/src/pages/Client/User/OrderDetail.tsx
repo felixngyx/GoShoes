8a5acf0d519ae192a13c0f547fc2c3ce06bc8a16
Felix <nguyenquockhanhmusic@gmail.com>
2024-11-29 11:40:52 +0700

    update detail

--- frontend/src/pages/Client/User/OrderDetail.tsx
+++ frontend/src/pages/Client/User/OrderDetail.tsx
@@ -190,31 +190,22 @@ const OrderDetail = () => {
                 <div className="flex justify-center w-full md:justify-start items-center space-x-4 py-8 border-b border-gray-200">
                   <div className="flex justify-start items-start flex-col space-y-2">
                     <p className="text-base font-semibold leading-4 text-left text-gray-800">
-                      {orderData.shipping.shipping_detail.name}
+                      {orderData.customer.name}
                     </p>
                     <p className="text-sm leading-5 text-gray-600">
-                      {orderData.customer.email}
+                      {orderData.customer.email || "Không có email"}
                     </p>
                   </div>
                 </div>
-
                 <div className="flex justify-center text-gray-800 md:justify-start items-center space-x-4 py-4 border-b border-gray-200 w-full">
                   <p className="text-sm leading-5">
-                    Phone: {orderData.shipping.shipping_detail.phone_number}
+                    Address: {JSON.parse(orderData.shipping.shipping_detail).address_detail}, {JSON.parse(orderData.shipping.shipping_detail).address}
                   </p>
                 </div>
-              </div>
-              <div className="flex justify-between xl:h-full items-stretch w-full flex-col mt-6 md:mt-0">
-                <div className="flex justify-center md:justify-start xl:flex-col flex-col md:space-x-6 lg:space-x-8 xl:space-x-0 space-y-4 xl:space-y-12 md:space-y-0 md:flex-row items-center md:items-start">
-                  <div className="flex justify-center md:justify-start items-center md:items-start flex-col space-y-4 xl:mt-8">
-                    <p className="text-base font-semibold leading-4 text-center md:text-left text-gray-800">
-                      Shipping Address
-                    </p>
-                    <p className="w-48 lg:w-full xl:w-48 text-center md:text-left text-sm leading-5 text-gray-600">
-                      {orderData.shipping.shipping_detail.address_detail},{" "}
-                      {orderData.shipping.shipping_detail.address}
-                    </p>
-                  </div>
+                <div className="flex justify-center text-gray-800 md:justify-start items-center space-x-4 py-4 border-b border-gray-200 w-full">
+                  <p className="text-sm leading-5">
+                    Phone: {orderData.shipping.shipping_detail.phone_number || "Number not available"}
+                  </p>
                 </div>
               </div>
             </div>
