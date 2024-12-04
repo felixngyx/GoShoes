8f4ac73613b3d799cf9ffe853910409f73e1b3e0
datnt11122004 <datnt11122004@gmail.com>
2024-12-04 22:47:30 +0700

    add query new

--- backend/app/Repositories/RepositoryInterfaces/UserRepositoryInterface.php
+++ backend/app/Repositories/RepositoryInterfaces/UserRepositoryInterface.php
@@ -4,7 +4,15 @@ namespace App\Repositories\RepositoryInterfaces;
 
 Interface UserRepositoryInterface
 {
-    public function paginate(int $perPage = 10, array $columns = ['*'], string $pageName = 'page', int $page = null);
+    public function paginate(
+        int $perPage = 10,
+        array $columns = ['*'],
+        string $pageName = 'page',
+        string $keyword = null,
+        int $page = null,
+        string $orderBy = 'created_at',
+        string $sortBy = 'asc'
+    );
     public function findByEmail(string $email);
 
 }
